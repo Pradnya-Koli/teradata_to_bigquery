8c7ceb6a99d87626d4e816ad249a95ada9baa47c
Pradnya-Koli <123149641+Pradnya-Koli@users.noreply.github.com>
2023-04-18 20:28:06 +0530

    Update schema_evolution.py

--- schema_evolution.py
+++ schema_evolution.py
@@ -1,3 +1,4 @@
+  GNU nano 4.8                                                                                                                                                                                                                                                                                                             schema_Evolution.py                                                                                                                                                                                                                                                                                                                        
 import teradatasql
 import json
 import pandas as pd
@@ -5,15 +6,24 @@ import re
 import logging
 from google.cloud import bigquery
 from Create_BigQuery_Table import *
-
-
+import os 
+from google.cloud import storage
+dataset_id = os.environ.get('dataset_name')
+project_id = os.environ.get('project_id')
+bucket_name = os.environ.get('bucket_name')
+host = os.environ.get('host')
+username = os.environ.get('username')
+password = os.environ.get('password')
+database =  "testdrive"
+bigquery_client = bigquery.Client(project=project_id)
+storage_client = storage.Client(project=project_id)
 def schema_evolution():
     try:
         project = 'searce-practice-data-analytics'
         client = bigquery.Client(project=project)
         logger = logging.getLogger(__name__)
         with open("config/teradata_config.json") as config_file:
-            open('schema_change/steradata_changes.sql', 'w').close()
+            open('/opt/teradata_to_bigquery/schema_changes/teradata_changes.sql', 'w').close()
             data = json.load(config_file)
             server = data['details']['host']
             database = data['details']['database']
@@ -30,38 +40,38 @@ def schema_evolution():
             print(result)
             result = str(result)
             result = result.replace("],", "\n")
-            f = open("schema_changes/teradata_changes.sql", "a+")
+            f = open("/opt/teradata_to_bigquery/schema_changes/teradata_changes.sql", "a+")
             f.write(result)
             f.close()
             word = 'UPDATE'
-            open('schema_changes/filter_changes.sql', 'w').close()
-            with open(r'schema_changes/teradata_changes.sql', 'r') as fp:
+            open('/opt/teradata_to_bigquery/schema_changes/filter_changes.sql', 'w').close()
+            with open(r'/opt/teradata_to_bigquery/schema_changes/teradata_changes.sql', 'r') as fp:
                 lines = fp.readlines()
                 for line in lines:
                     if line.find(word) != -1:
                         queries = str(line)
                         queries = queries.replace('"', '')
                         print(queries)
-                        op = open('filter_changes.sql', 'a+')
+                        op = open('/opt/teradata_to_bigquery/schema_changes/filter_changes.sql', 'a+')
                         op.write(queries)
                         op.close()
 
             word = 'ALTER'
             #            open('filter_changes.sql', 'w').close()
-            with open(r'schema_changes/teradata_changes.sql', 'r') as fp:
+            with open(r'/opt/teradata_to_bigquery/schema_changes/teradata_changes.sql', 'r') as fp:
                 lines = fp.readlines()
                 for line in lines:
                     if line.find(word) != -1:
                         queries = str(line)
 
                         print(queries)
-                        op = open('filter_changes.sql', 'a+')
+                        op = open('/opt/teradata_to_bigquery/schema_changes/filter_changes.sql', 'a+')
                         op.write(queries)
                         op.close()
 
-            open('Source_DDL/source_ddl.sql', 'w').close()
+            open('/opt/teradata_to_bigquery/Source_DDL/source_ddl.sql', 'w').close()
             words = 'None'
-            with open(r'schema_changes/filter_changes.sql', 'r') as rd:
+            with open(r'/opt/teradata_to_bigquery/schema_changes/filter_changes.sql', 'r') as rd:
                 lines = rd.readlines()
                 for line in lines:
                     if line.find(words) != -1:
@@ -69,15 +79,31 @@ def schema_evolution():
                         success_query = success_query.replace("[", "").replace(", None", "").replace("'", "")
                         print(success_query)
 
-                        succes = open('Source_DDL/source_ddl.sql', 'a+')
+                        succes = open('/opt/teradata_to_bigquery/Source_DDL/source_ddl.sql', 'a+')
                         succes.write(success_query)
                         succes.close()
+ #           with open(r'/opt/teradata_to_bigquery/Source_DDL/source_ddl.sql', 'r') as rd:
+#            word='UPDATE'
+
+
     except Exception as e:
         logger.exception('Failed to open database connection.')
         print("Failed")
 
 
+def formatting_update():
+    with open('/opt/teradata_to_bigquery/BigQuery_Compatible_DDL/source_ddl.sql', 'r') as rd:
+        lines = rd.readlines()
+        words ='UPDATE'
+        for line in lines:
+            if line.find(words) != -1:
+               format_query=str(line)
+               replace_query=("{}").format(database)
+               format_query=format_query.replace(replace_query,"")
+               final_update_query = open('/opt/teradata_to_bigquery/BigQuery_Compatible_DDL/source_ddl.sql', 'a+')
+
+               final_update_query.write(format_query)
+               final_update_query.close()
+
 schema_evolution()
-
-
 
